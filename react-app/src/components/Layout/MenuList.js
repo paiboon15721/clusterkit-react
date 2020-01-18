d8db88bd3b81e74994e7eb3b62c7ba9af3e11eb6
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-18 14:15:52 +0700

    update

--- react-app/src/components/Layout/MenuList.js
+++ react-app/src/components/Layout/MenuList.js
@@ -1,23 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const MenuList = () => {
+  const { pathname } = useLocation();
+
   return (
     <nav className="col-md-2 d-none d-md-block bg-light sidebar">
       <div className="sidebar-sticky">
         <ul className="nav flex-column">
           <li className="nav-item">
-            <Link to="/dashboard" className="nav-link active">
+            <Link
+              to="/dashboard"
+              className={`nav-link${
+                pathname === "/dashboard" ? " active" : ""
+              }`}
+            >
               Dashboard
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/login-app" className="nav-link active">
+            <Link
+              to="/login-app"
+              className={`nav-link${
+                pathname === "/login-app" ? " active" : ""
+              }`}
+            >
               Login App
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/comment-app" className="nav-link active">
+            <Link
+              to="/comment-app"
+              className={`nav-link${
+                pathname === "/comment-app" ? " active" : ""
+              }`}
+            >
               Comment App
             </Link>
           </li>
