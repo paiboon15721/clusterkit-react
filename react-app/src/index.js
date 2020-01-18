50e8112e6c0cbf3a4538a8f1cd046908a8c17e89
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-18 10:33:11 +0700

    update

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => (
-  <div className="container pb-4">
-    <div className="form-group">
-      <input placeholder="username" className="form-control" />
+const App = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUserNameChange = e => {
+    setUserName(e.target.value);
+  };
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const result = {
+      userName: userName,
+      password: password
+    };
+
+    console.log(result);
+  };
+
+  return (
+    <div className="container">
+      <div className="mt-4">
+        <div className="form-group">
+          <input
+            value={userName}
+            onChange={handleUserNameChange}
+            placeholder="username"
+            className="form-control"
+          />
+        </div>
+        <div value={password} className="form-group">
+          <input
+            onChange={handlePasswordChange}
+            placeholder="password"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <button onClick={handleSubmit} className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
-    <div className="form-group">
-      <input placeholder="password" className="form-control" />
-    </div>
-    <div className="form-group">
-      <button className="btn btn-primary">Submit</button>
-    </div>
-  </div>
-);
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
