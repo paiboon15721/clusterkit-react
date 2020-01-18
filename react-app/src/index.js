5c95a390072d37d69920b62cc53f42b61f49cde4
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-18 13:24:49 +0700

    update

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,18 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import LoginApp from "./LoginApp";
 import CommentApp from "./CommentApp";
 
 const App = () => (
   <div className="container">
     <Router>
-      <Route path="/login-app">
-        <LoginApp name="Login App" />
-      </Route>
-      <Route path="/comment-app">
-        <CommentApp />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login-app" />
+        </Route>
+        <Route path="/login-app">
+          <LoginApp name="Login App" />
+        </Route>
+        <Route path="/comment-app">
+          <CommentApp />
+        </Route>
+      </Switch>
     </Router>
   </div>
 );
