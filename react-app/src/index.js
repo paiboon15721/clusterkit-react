c9c3b4e8413bc1f7bc0cefebd268e1e1f64201f1
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-18 15:05:41 +0700

    update

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -11,13 +11,15 @@ import CommentApp from "./pages/CommentApp";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import UserTable from "./pages/UserTable";
+import ReactUse from "./pages/ReactUse";
 import "antd/dist/antd.css";
 
 export const menus = [
   { path: "/dashboard", name: "Dashboard", component: Dashboard },
   { path: "/login-app", name: "Login App", component: LoginApp },
   { path: "/comment-app", name: "Comment App", component: CommentApp },
-  { path: "/user-table", name: "User Table", component: UserTable }
+  { path: "/user-table", name: "User Table", component: UserTable },
+  { path: "/react-use", name: "React Use", component: ReactUse }
 ];
 
 const App = () => (
